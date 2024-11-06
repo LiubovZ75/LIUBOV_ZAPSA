9a64ded2379cb3b6ecbce77f660a45396305a9ac
Liubov <bavazapsha@gmail.com>
2024-11-06 17:04:22 +0200

    HomeWork2

---
+++ src/main/java/homework_nr_2/HomeWork2.java
@@ -0,0 +1,28 @@
+package homework_nr_2;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        long y =  7811080815L;
+        System.out.println("CurrentWorldPopulation = " + y);
+        int num = 55235358;
+        System.out.println("CarsProducedThisYear =" + " 55235358");
+        char x = 'A' ;
+        System.out.println("FirstLetterInAlphabet =" + " A" );
+        boolean JavaIsAmazing = true;
+        System.out.println("JavaIsAmazing =" + JavaIsAmazing);
+        double  a = 3.14159;
+        System.out.println("Pi" + " =" + a);
+       double price = 21.34d;
+        System.out.println("Price типа double = " + price);
+        double tax = 112.54d;
+        System.out.println("Tax типа double = " + tax);
+        int quan = 150;
+        System.out.println("Quantity типа int = " + quan);
+        double sum = 0;
+        System.out.println("Sum типа double = " + sum);
+        double total  ;
+        total = price + tax * quan + sum;
+        System.out.println("Total = " + total);
+
+    }
+}
